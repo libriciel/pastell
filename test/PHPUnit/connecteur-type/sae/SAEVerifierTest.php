b3c84d80569f0cbefc5413c9759d6bd31eb6ee21
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2024-09-17 10:42:28 +0200

    fix urlencode

--- test/PHPUnit/connecteur-type/sae/SAEVerifierTest.php
+++ test/PHPUnit/connecteur-type/sae/SAEVerifierTest.php
@@ -6,10 +6,6 @@ class SAEVerifierTest extends PastellTestCase
 {
     use CurlUtilitiesTestTrait;
 
-    private const MESSAGE_ACK = 'https://sae/sedaMessages/sequence:ArchiveTransfer/message:Acknowledgement/' .
-    'originOrganizationIdentification:LS_PA/' .
-    'originMessageIdentifier:15ef78ef665a8777c33d1125783707f8dfb190f82869dc9248e46c5ed396d70b_1542893421';
-
     /**
      * @throws NotFoundException
      */
@@ -76,7 +72,12 @@ class SAEVerifierTest extends PastellTestCase
     private function mockCurlWithAckResponse(string $ack_response_filename): void
     {
         $this->mockCurl([
-            self::MESSAGE_ACK => file_get_contents($ack_response_filename),
+            'https://' . urlencode(
+                'sae/sedaMessages/sequence:ArchiveTransfer/message:Acknowledgement/' .
+                'originOrganizationIdentification:LS_PA/' .
+                'originMessageIdentifier:15ef78ef665a8777c33d1125783707f8dfb190f82869dc9248e46c5ed396d70b_1542893421'
+            )
+            => file_get_contents($ack_response_filename),
         ]);
     }
 
