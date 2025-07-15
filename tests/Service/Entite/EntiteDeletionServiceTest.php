ecc243555b2e370b52abcf3e99cdd022953bb325
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2025-07-15 10:31:29 +0200

    fix tests

--- tests/Service/Entite/EntiteDeletionServiceTest.php
+++ tests/Service/Entite/EntiteDeletionServiceTest.php
@@ -59,7 +59,12 @@ class EntiteDeletionServiceTest extends PastellTestCase
 
     public function testIsNotSupprimableBecauseHasEntiteFille(): void
     {
-        self::getObjectInstancier()->getInstance(EntiteSQL::class)->create('name-fake', 'siren-fake', EntiteSQL::TYPE_COLLECTIVITE, $this->id_entity_exemple);
+        self::getObjectInstancier()->getInstance(EntiteSQL::class)->create(
+            'name-fake',
+            'siren-fake',
+            EntiteSQL::TYPE_COLLECTIVITE,
+            $this->id_entity_exemple
+        );
 
         $isSupprimable = $this->entiteDeletionService->canDelete($this->id_entity_exemple);
         self::assertFalse($isSupprimable);
@@ -67,7 +72,10 @@ class EntiteDeletionServiceTest extends PastellTestCase
 
     public function testIsNotSupprimableBecauseHasUsersWithEntiteDeBase(): void
     {
-        self::getObjectInstancier()->getInstance(UtilisateurSQL::class)->query('UPDATE utilisateur SET id_e = ? WHERE id_u = 1', $this->id_entity_exemple);
+        self::getObjectInstancier()->getInstance(UtilisateurSQL::class)->query(
+            'UPDATE utilisateur SET id_e = ? WHERE id_u = 1',
+            $this->id_entity_exemple
+        );
 
         $isSupprimable = $this->entiteDeletionService->canDelete($this->id_entity_exemple);
         self::assertFalse($isSupprimable);
@@ -77,7 +85,11 @@ class EntiteDeletionServiceTest extends PastellTestCase
     {
         $id_d = 'IDENTIFIANT-FAKE';
         self::getObjectInstancier()->getInstance(DocumentSQL::class)->save($id_d, 'type');
-        self::getObjectInstancier()->getInstance(DocumentEntite::class)->addRole($id_d, $this->id_entity_exemple, 'ROLE-FAKE');
+        self::getObjectInstancier()->getInstance(DocumentEntite::class)->addRole(
+            $id_d,
+            $this->id_entity_exemple,
+            'ROLE-FAKE'
+        );
 
         $isSupprimable = $this->entiteDeletionService->canDelete($this->id_entity_exemple);
         self::assertFalse($isSupprimable);
@@ -85,7 +97,11 @@ class EntiteDeletionServiceTest extends PastellTestCase
 
     public function testIsNotSupprimableBecauseHasUser(): void
     {
-        self::getObjectInstancier()->getInstance(RoleUtilisateur::class)->addRole(1, 'role-fake', $this->id_entity_exemple);
+        self::getObjectInstancier()->getInstance(RoleUtilisateur::class)->addRole(
+            1,
+            'role-fake',
+            $this->id_entity_exemple
+        );
 
         $isSupprimable = $this->entiteDeletionService->canDelete($this->id_entity_exemple);
         self::assertFalse($isSupprimable);
@@ -93,7 +109,13 @@ class EntiteDeletionServiceTest extends PastellTestCase
 
     public function testIsNotSupprimableBecauseHasConnector(): void
     {
-        self::getObjectInstancier()->getInstance(ConnecteurEntiteSQL::class)->addConnecteur($this->id_entity_exemple, 4, 'type-fake', 'libelle-fake');
+        self::getObjectInstancier()->getInstance(ConnecteurEntiteSQL::class)->addConnecteur(
+            $this->id_entity_exemple,
+            4,
+            'type-fake',
+            'libelle-fake',
+            0
+        );
 
         $isSupprimable = $this->entiteDeletionService->canDelete($this->id_entity_exemple);
         self::assertFalse($isSupprimable);
@@ -101,7 +123,12 @@ class EntiteDeletionServiceTest extends PastellTestCase
 
     public function testIsNotSupprimableBecauseHasfluxEntity(): void
     {
-        self::getObjectInstancier()->getInstance(FluxEntiteSQL::class)->addConnecteur($this->id_entity_exemple, 'flux-fake', 'type-fake', 4);
+        self::getObjectInstancier()->getInstance(FluxEntiteSQL::class)->addConnecteur(
+            $this->id_entity_exemple,
+            'flux-fake',
+            'type-fake',
+            4
+        );
 
         $isSupprimable = $this->entiteDeletionService->canDelete($this->id_entity_exemple);
         self::assertFalse($isSupprimable);
@@ -109,7 +136,10 @@ class EntiteDeletionServiceTest extends PastellTestCase
 
     public function testIsNotSupprimableBecauseHasfluxEntityHeritage(): void
     {
-        self::getObjectInstancier()->getInstance(FluxEntiteHeritageSQL::class)->setInheritance($this->id_entity_exemple, 'flux-fake');
+        self::getObjectInstancier()->getInstance(FluxEntiteHeritageSQL::class)->setInheritance(
+            $this->id_entity_exemple,
+            'flux-fake'
+        );
 
         $isSupprimable = $this->entiteDeletionService->canDelete($this->id_entity_exemple);
         self::assertFalse($isSupprimable);
