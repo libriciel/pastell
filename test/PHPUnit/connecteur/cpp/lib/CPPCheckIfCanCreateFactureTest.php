bc48e19fcecf8bc189956dff32717cba1f33ca3b
cdutheil <cdutheil@libriciel.coop>
2025-07-16 15:00:29 +0200

    #2273 - replacement d'un fichier deplacé par erreur

--- test/PHPUnit/connecteur/cpp/lib/CPPCheckIfCanCreateFactureTest.php
+++ test/PHPUnit/connecteur/cpp/lib/CPPCheckIfCanCreateFactureTest.php
@@ -1,6 +1,11 @@
 <?php
 
+namespace PHPUnit\connecteur\cpp\lib;
+
+use CPPCheckIfCanCreateFacture;
+use ExtensionCppTestCase;
 use Pastell\Service\ChorusPro\ChorusProImportUtilService;
+use PortailFactureConnecteur;
 
 class CPPCheckIfCanCreateFactureTest extends ExtensionCppTestCase
 {
