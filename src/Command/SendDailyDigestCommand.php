40b2c300c9f56c9d1c9a8507b6ee63839e61ab00
lesclapez <lesclapez@libriciel.coop>
2025-09-03 17:33:22 +0200

    replace batch scripts by commands

---
+++ src/Command/SendDailyDigestCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pastell\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Throwable;
+
+#[AsCommand(
+    name: 'app:notification:daily-digest',
+    description: 'Envoie le récapitulatif quotidien par e-mail (équivalent du script CLI).',
+)]
+final class SendDailyDigestCommand extends Command
+{
+    public function __construct(
+        private readonly \NotificationMail $notificationMail,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        try {
+            $this->notificationMail->sendDailyDigest();
+            $io->success('Récapitulatif quotidien envoyé.');
+            return Command::SUCCESS;
+        } catch (Throwable $e) {
+            $io->error('Échec de l’envoi : ' . $e->getMessage());
+            return Command::FAILURE;
+        }
+    }
+}
