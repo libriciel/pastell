113b83874e4b232f623dc80fea0b85ab3f1812e0
iruiz <ines.ruiz@libriciel.coop>
2025-10-17 10:41:28 +0200

    delete ActionChange::updateModification #2336

--- pastell-core/ActionChange.php
+++ pastell-core/ActionChange.php
@@ -1,36 +1,21 @@
 <?php
 
-class ActionChange extends SQL
+declare(strict_types=1);
+
+final readonly class ActionChange
 {
     public function __construct(
-        private readonly DocumentActionSQL $documentActionSQL,
-        private readonly DocumentActionEntite $documentActionEntite,
-        private readonly Journal $journal,
-        private readonly SQLQuery $sqlQuery
+        private DocumentActionSQL    $documentActionSQL,
+        private DocumentActionEntite $documentActionEntite,
+        private Journal              $journal,
     ) {
-        parent::__construct($this->sqlQuery);
     }
 
-    public function addAction($id_d, $id_e, $id_u, $action, $message_journal): void
+    public function addAction($id_d, $id_e, $id_u, string $action, string $message_journal): void
     {
         $id_a = $this->documentActionSQL->add($id_d, $id_e, $id_u, $action);
         $id_j = $this->journal->addSQL(Journal::DOCUMENT_ACTION, $id_e, $id_u, $id_d, $action, $message_journal);
         $this->documentActionEntite->add($id_a, $id_e, $id_j);
-    }
-
-    /**
-     * @deprecated 4.0.26 Use addOrUpdateAction instead
-     */
-    public function updateModification($id_d, $id_e, $id_u, $action)
-    {
-        $document_action = $this->documentActionSQL->getLastActionInfo($id_d, $id_e);
-
-        if (! $document_action || $document_action['id_u'] != $id_u || $document_action['action'] != $action) {
-            $this->addAction($id_d, $id_e, $id_u, $action, "Modification du document");
-        } else {
-            $this->documentActionSQL->updateDate($document_action['id_a']);
-            $this->journal->addSQL(Journal::DOCUMENT_ACTION, $id_e, $id_u, $id_d, $action, "Modification du document");
-        }
     }
 
     public function addOrUpdateAction(
