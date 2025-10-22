33aa5c25e33ff5a3828b030f210c64117c716b4b
iruiz <ines.ruiz@libriciel.coop>
2025-10-22 17:27:10 +0200

    Suppression d'éléments dépréciés de util.php #2347

--- util.php
+++ util.php
@@ -6,7 +6,7 @@
 
 function get_hecho(?string $message = '', int $quote_style = ENT_QUOTES): string
 {
-    return htmlentities($message ?? '', $quote_style, "utf-8");
+    return htmlentities($message ?? '', $quote_style, 'utf-8');
 }
 
 function hecho(?string $message = '', int $quot_style = ENT_QUOTES): void
@@ -17,61 +17,9 @@ function hecho(?string $message = '', int $quot_style = ENT_QUOTES): void
 function getDateIso($value)
 {
     if (!$value) {
-        return "";
+        return '';
     }
-    return preg_replace("#^(\d{2})/(\d{2})/(\d{4})$#", '$3-$2-$1', $value);
-}
-
-
-/**
- * @deprecated 4.0.8
- */
-function rrmdir($dir)
-{
-    if (!is_dir($dir)) {
-        return;
-    }
-    foreach (scandir($dir) as $object) {
-        if (in_array($object, [".", ".."])) {
-            continue;
-        }
-        if (is_dir("$dir/$object")) {
-            rrmdir("$dir/$object");
-        } else {
-            unlink("$dir/$object");
-        }
-    }
-    rmdir($dir);
-}
-
-/**
- * @deprecated 4.0.8
- */
-function get_argv($num_arg)
-{
-    global $argv;
-    if (empty($argv[$num_arg])) {
-        return false;
-    }
-    return $argv[$num_arg];
-}
-
-/**
- * @deprecated 4.0.8
- */
-function exceptionToJson(Exception $ex)
-{
-    $json = [
-        'date' => date('d/m/Y H:i:s'),
-        'code' => $ex->getCode(),
-        'file' => $ex->getFile(),
-        'line' => $ex->getLine(),
-        'message' => $ex->getMessage(),
-        // utf8_encode_array non applicable sur getTrace() car peut contenir des "resources"
-        'trace' => explode("\n", $ex->getTraceAsString()),
-    ];
-    $json = json_encode($json);
-    return $json;
+    return preg_replace('#^(\d{2})/(\d{2})/(\d{4})$#', '$3-$2-$1', $value);
 }
 
 function date_iso_to_fr($date)
@@ -79,45 +27,17 @@ function date_iso_to_fr($date)
     if (!$date) {
         return '';
     }
-    return date("d/m/Y", strtotime($date));
+    return date('d/m/Y', strtotime($date));
 }
 
 function time_iso_to_fr($datetime)
 {
-    return date("d/m/Y H:i:s", strtotime($datetime));
+    return date('d/m/Y H:i:s', strtotime($datetime));
 }
 
 function date_fr_to_iso($date)
 {
-    return preg_replace("#^(\d{2})/(\d{2})/(\d{4})$#", '$3-$2-$1', $date);
-}
-
-/**
- * @throws Exception
- * @deprecated 4.0.8
- */
-function throwIfFalse($result, $message = false)
-{
-    if ($result === false) {
-        throwLastError($message);
-    }
-    return $result;
-}
-
-/**
- * @throws Exception
- * @deprecated 4.0.8
- */
-function throwLastError($message = false)
-{
-    $last = error_get_last();
-    $cause = $last['message'];
-    if ($message) {
-        $ex = $message . ' Cause : ' . $cause;
-    } else {
-        $ex = $cause;
-    }
-    throw new Exception($ex);
+    return preg_replace('#^(\d{2})/(\d{2})/(\d{4})$#', '$3-$2-$1', $date);
 }
 
 function header_wrapper($str)
@@ -148,10 +68,10 @@ function exit_wrapper(int $code = 0): never
  */
 function setcookie_wrapper(
     $name,
-    $value = "",
+    $value = '',
     $expire = 0,
-    $path = "",
-    $domain = "",
+    $path = '',
+    $domain = '',
     $secure = false,
     $httponly = false
 ) {
@@ -174,29 +94,6 @@ function move_uploaded_file_wrapper($filename, $destination)
     return move_uploaded_file($filename, $destination);
 }
 
-/**
- * @deprecated 4.0.8
- */
-function wl_basename($file)
-{
-    $fileInArray = explode(DIRECTORY_SEPARATOR, $file);
-    return end($fileInArray);
-}
-
-/**
- * @deprecated 4.0.8
- */
-function tick()
-{
-    static $tick;
-    $microtime = microtime(true);
-    if ($tick) {
-        echo intval(($microtime - $tick) * 1000);
-        echo "\n";
-    }
-    $tick = $microtime;
-}
-
 function utf8_encode_array($array)
 {
     if (!is_array($array) && !is_object($array)) {
@@ -209,22 +106,7 @@ function utf8_encode_array($array)
     return $result;
 }
 
-/**
- * @deprecated 4.0.8
- */
-function utf8_decode_array($array)
+function number_format_fr($number): string
 {
-    if (!is_array($array) && !is_object($array)) {
-        return utf8_decode($array);
-    }
-    $result = [];
-    foreach ($array as $cle => $value) {
-        $result[utf8_decode($cle)] = utf8_decode_array($value);
-    }
-    return $result;
-}
-
-function number_format_fr($number)
-{
-    return number_format($number, 0, ",", " ");
+    return number_format($number, 0, ',', ' ');
 }
