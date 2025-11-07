2e866421b18cb1d61aaec7b294d03228f6873867
lesclapez <lesclapez@libriciel.coop>
2025-11-07 16:24:54 +0100

    Gray out role when editing
    lint

--- template/RoleEdition.php
+++ template/RoleEdition.php
@@ -14,22 +14,21 @@
     <form class="form-horizontal" action='<?php $this->url('Role/doEdition'); ?>' method='post'>
         <?php $this->getCSRFToken()->displayFormInput() ?>
         <input type='hidden' name='nouveau' value='<?php hecho($nouveau); ?>'/>
-        <?php if ($role_info['role'] !== '') : ?>
-            <input type="hidden" name="role" value="<?php
-            hecho($role_info['role']); ?>"/>
-        <?php else : ?>
-            <div class="control-group">
-                <label class="control-label" for="role">Rôle<span class="obl">*</span></label>
-                <div class="controls">
-                    <input class="form-control col-md-4"
-                           type="text"
-                           name="role"
-                           id="role"
-                           value=""
-                    />
-                </div>
+        <div class="control-group">
+            <label class="control-label" for="role">Rôle<span class="obl">*</span></label>
+            <div class="controls">
+                <input class="form-control col-md-4"
+                       type="text"
+                       name="role"
+                       id="role"
+                    <?php
+                    if ($role_info['role'] !== '') {
+                        echo 'disabled="disabled"';
+                    } ?>
+                       value="<?php hecho($role_info['role']); ?>"
+                />
             </div>
-        <?php endif; ?>
+        </div>
 
         <div class="control-group">
             <label class="control-label" for="libelle">Libellé<span class="obl">*</span></label>
