783965ebf70944e070b21c97eecb8bd219827f08
lesclapez <lesclapez@libriciel.coop>
2025-11-12 11:13:16 +0100

    Review fix

--- template/RoleEdition.php
+++ template/RoleEdition.php
@@ -7,6 +7,7 @@
  * @var string $cancelRedirectUrl
  */
 
+$edition_role = $role_info['role'] !== '';
 ?>
 
 <div class="box">
@@ -15,21 +16,25 @@
         <?php $this->getCSRFToken()->displayFormInput() ?>
         <input type='hidden' name='nouveau' value='<?php hecho($nouveau); ?>'/>
         <div class="control-group">
-            <label class="control-label" for="role">Rôle</label>
+            <label class="control-label" for="role">Rôle
+                <?php if (!$edition_role) : ?>
+                    <span class="obl">*</span>
+                <?php endif; ?>
+            </label>
             <div class="controls">
                 <input class="form-control col-md-4"
                        type="text"
                        name="role"
                        id="role"
                     <?php
-                    if ($role_info['role'] !== '') {
+                    if ($edition_role) {
                         echo 'disabled="disabled"';
                     } ?>
                        value="<?php hecho($role_info['role']); ?>"
                 />
             </div>
         </div>
-        <?php if ($role_info['role'] !== '') : ?>
+        <?php if ($edition_role) : ?>
             <input type="hidden" name="role" value="<?php hecho($role_info['role']); ?>" />
         <?php endif; ?>
 
