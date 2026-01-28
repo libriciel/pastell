ab4d42ce36d7285b500c15e7811d687b2b13eb92
lesclapez <lesclapez@libriciel.coop>
2026-01-28 16:54:07 +0100

    update search.js for button visibility

--- web/js/search.js
+++ web/js/search.js
@@ -16,12 +16,9 @@ $(document).ready(function() {
     return decodeURI(results[1]) || 0;
 }
 
-    if($.urlParamExists("search") || $.urlParamExists("recherche")) {
-      $("#headingOne").addClass("collapsed");
-      $("#collapseOne").removeClass("show");
-
-      $("#title-result").addClass("ls-on");
-      $("#title-result").removeClass("ls-off")
-    }
+$("#headingOne").addClass("collapsed");
+$("#collapseOne").removeClass("show");
+$("#title-result").addClass("ls-on");
+$("#title-result").removeClass("ls-off")
 
 });
