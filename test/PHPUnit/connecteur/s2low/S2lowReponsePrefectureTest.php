5d6942c8717552751593162c475f4b73d7646387
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2026-04-28 18:22:12 +0200

    use CurlUtilitiesTestTrait in S2lowReponsePrefectureTest

--- test/PHPUnit/connecteur/s2low/S2lowReponsePrefectureTest.php
+++ test/PHPUnit/connecteur/s2low/S2lowReponsePrefectureTest.php
@@ -1,33 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 class S2lowReponsePrefectureTest extends PastellTestCase
 {
+    use CurlUtilitiesTestTrait;
+
     /**
-     * @param $curl_response
-     * @return Connecteur
+     * @throws DonneesFormulaireException
      * @throws Exception
      */
-    private function getS2low($curl_response)
+    private function getS2low(string $curlResponse): S2low
     {
-        $curlWrapper = $this->createMock(CurlWrapper::class);
-
-        $curlWrapper
-            ->method('get')
-            ->willReturn($curl_response);
-
-        $curlWrapperFactory = $this->createMock(CurlWrapperFactory::class);
-
-        $curlWrapperFactory
-            ->method('getInstance')
-            ->willReturn($curlWrapper);
-
-        $this->getObjectInstancier()->setInstance(CurlWrapperFactory::class, $curlWrapperFactory);
+        $this->mockCurl([
+            '/admin/users/api-list-login.php' => true,
+            '/modules/actes/actes_transac_reponse_create.php' => $curlResponse,
+        ]);
 
         $info = $this->createConnector('s2low', 'S2LOW');
 
         $collectiviteProperties = $this->getDonneesFormulaireFactory()->getConnecteurEntiteFormulaire($info['id_ce']);
-        $collectiviteProperties->addFileFromCopy("classification_file", "classification.xml", __DIR__ . "/fixtures/classification-exemple.xml");
+        $collectiviteProperties->addFileFromCopy(
+            'classification_file',
+            'classification.xml',
+            __DIR__ . '/fixtures/classification-exemple.xml'
+        );
 
+        /** @var S2low */
         return $this->getConnecteurFactory()->getConnecteurById($info['id_ce']);
     }
 
@@ -36,9 +35,8 @@ class S2lowReponsePrefectureTest extends PastellTestCase
      * @throws UnrecoverableException
      * @throws Exception
      */
-    public function testSendReponseType()
+    public function testSendReponseType(): void
     {
-        /** @var S2low $s2low */
         $s2low = $this->getS2low("OK\n52");
 
         $donneesFormulaire = $this->getDonneesFormulaireFactory()->getNonPersistingDonneesFormulaire();
@@ -46,15 +44,15 @@ class S2lowReponsePrefectureTest extends PastellTestCase
             'type_reponse' => TdtConnecteur::DEMANDE_PIECE_COMPLEMENTAIRE,
             'type_acte_demande_piece_complementaire' => '99_AI',
             'acte_nature' => 3,
-            'type_pj_demande_piece_complementaire' => json_encode(['99_AU','AA_11']),
+            'type_pj_demande_piece_complementaire' => json_encode(['99_AU', 'AA_11'], JSON_THROW_ON_ERROR),
         ]);
 
-        $donneesFormulaire->addFileFromData("reponse", "foo.pdf", "bar");
-        $donneesFormulaire->addFileFromData("reponse_pj_demande_piece_complementaire", "baz.pdf", "buz", 0);
-        $donneesFormulaire->addFileFromData("reponse_pj_demande_piece_complementaire", "baz2.pdf", "buz2", 1);
+        $donneesFormulaire->addFileFromData('reponse', 'foo.pdf', 'bar');
+        $donneesFormulaire->addFileFromData('reponse_pj_demande_piece_complementaire', 'baz.pdf', 'buz', 0);
+        $donneesFormulaire->addFileFromData('reponse_pj_demande_piece_complementaire', 'baz2.pdf', 'buz2', 1);
 
         $s2low->sendResponse($donneesFormulaire);
 
-        $this->assertEquals(52, $donneesFormulaire->get('response_transaction_id'));
+        static::assertSame('52', $donneesFormulaire->get('response_transaction_id'));
     }
 }
