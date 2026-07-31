e1886063b1b50b0824c3e2ccd50c120754bdb03e
Loïc ESCLAPEZ <lesclapez@libriciel.coop>
2026-07-31 11:50:14 +0200

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Ines RUIZ <iruiz@libriciel.coop>

--- controler/UtilisateurControler.php
+++ controler/UtilisateurControler.php
@@ -355,10 +355,9 @@ class UtilisateurControler extends PastellControler
         if (!$this->hasDroitFor($info['id_e'], DroitService::DROIT_UTILISATEUR, DroitType::LECTURE)) {
             $this->setLastError(
                 \sprintf(
-                    "Vous n'avez pas les droits nécessaires (%s:%s:%s) pour accéder à cette page",
+                    "Vous n'avez pas les droits nécessaires (%s:%s) pour accéder à cette page",
                     $info['id_e'],
-                    DroitService::DROIT_UTILISATEUR,
-                    DroitType::LECTURE->value
+                    DroitService::getDroitFor(DroitService::DROIT_UTILISATEUR, DroitType::LECTURE)
                 )
             );
             $this->redirect();
