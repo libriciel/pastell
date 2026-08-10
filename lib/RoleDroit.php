59e89f24f660811274e2a3d3b508e2f5b86637f5
lesclapez <lesclapez@libriciel.coop>
2026-08-10 10:17:04 +0200

    deprecation

--- lib/RoleDroit.php
+++ lib/RoleDroit.php
@@ -35,6 +35,9 @@ class RoleDroit
         return array_merge($droit, $this->documentTypeFactory->getAllDroit());
     }
 
+    /**
+     * @deprecated 4.1.22 no longer used, to be deleted in 6.0.0
+     */
     public function areExistingRolesDroits(array $rolesDroits): bool
     {
         return count(array_intersect($rolesDroits, $this->getAllDroit())) === count($rolesDroits);
