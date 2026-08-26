acd106ba4ea4060cd3def6383bdfe7c9ca9529a1
lesclapez <lesclapez@libriciel.coop>
2026-08-26 09:59:32 +0200

    review

--- connecteur/s2low/S2low.php
+++ connecteur/s2low/S2low.php
@@ -752,7 +752,13 @@ class S2low extends TdtConnecteur
         mkdir($result_folder);
 
         $command = "tar -zxvf $temp_file_path --directory $result_folder 2>&1";
-        exec($command);
+        exec($command, $output, $return_var);
+        if ($return_var !== 0) {
+            $this->getLogger()->error(
+                "Impossible d'extraire l'archive de la réponse de la préfecture (id_d=$new_id_d)",
+                [$command, $output]
+            );
+        }
 
         $file_list = scandir($result_folder);
         $num_file = 0;
