3ed013e484d2453ebb8c9c49b2ca22397d41b8f6
iruiz <ines.ruiz@libriciel.coop>
2026-09-15 10:57:14 +0200

    review #964

--- src/Model/Daemon/JobAdvancedFilters.php
+++ src/Model/Daemon/JobAdvancedFilters.php
@@ -27,9 +27,14 @@ final readonly class JobAdvancedFilters
         return new self(
             type: (array)$recuperateur->get('job_type', []),
             job_status: (array)$recuperateur->get('job_status', []),
-            id_e: $recuperateur->get('search_id_e', ''),
-            include_children: $recuperateur->get('include_children', ''),
+            id_e: self::toScalarString($recuperateur->get('search_id_e', '')),
+            include_children: self::toScalarString($recuperateur->get('include_children', '')),
             id_verrou: (array)$recuperateur->get('id_verrou', []),
         );
+    }
+
+    private static function toScalarString(mixed $value): string
+    {
+        return \is_array($value) ? '' : (string)$value;
     }
 }
